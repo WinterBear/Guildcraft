d49344a164e59ea4b5e8d6fbff7a564bb42febd7
James Reynolds <jamesreynoldsjobs@gmail.com>
2022-04-04 22:22:32 +0100

    Fix Join Command

--- src/main/java/dev/snowcave/guilds/commands/invites/GuildJoinCommandHandler.java
+++ src/main/java/dev/snowcave/guilds/commands/invites/GuildJoinCommandHandler.java
@@ -1,11 +1,13 @@
 package dev.snowcave.guilds.commands.invites;
 
 import dev.snowcave.guilds.Guilds;
+import dev.snowcave.guilds.commands.base.GuildCommandHandler;
 import dev.snowcave.guilds.commands.base.GuildMemberCommandHandler;
 import dev.snowcave.guilds.core.Guild;
 import dev.snowcave.guilds.core.users.User;
 import dev.snowcave.guilds.utils.conversations.Invite;
 import io.github.winterbear.WinterCoreUtils.ChatUtils;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
@@ -15,14 +17,14 @@ import java.util.Optional;
 /**
  * Created by WinterBear on 28/12/2020.
  */
-public class GuildJoinCommandHandler extends GuildMemberCommandHandler {
+public class GuildJoinCommandHandler implements GuildCommandHandler {
 
     @Override
-    public void handle(Player player, User user, String[] arguments) {
+    public void handle(Player player, String[] arguments) {
         if(arguments.length < 2){
-            ChatUtils.send(player, ChatUtils.format("&b/guild join &e<&6Guild&e> &8- &7Invite a player to join your Guild."));
+            ChatUtils.send(player, ChatUtils.format("&b/guild invite &e<&6Guild&e> &8- &7Invite a player to join your Guild."));
         } else {
-            String guildName = String.join(" ", Arrays.copyOfRange(arguments, 2, arguments.length));
+            String guildName = String.join(" ", Arrays.copyOfRange(arguments, 1, arguments.length));
             Optional<Guild> guild = Guilds.getGuild(guildName);
             if(guild.isPresent()){
                 Optional<Invite> invite = GuildInviteCommandHandler.INVITES.stream()
